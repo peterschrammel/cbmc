b641c1a2a84686c52f173f1226dfbf9931d85c27
thk123 <thomas@diffblue.com>
2016-11-21 15:35:05 +0000

    Moved the function for generating the tests into c_test_case_generator The division between the two files was unnecessary as using separate classes to handle specific parts of the generation.

--- src/test-c-gen/c_test_case_generator.cpp
+++ src/test-c-gen/c_test_case_generator.cpp
@@ -12,34 +12,14 @@
 
 #include <cbmc/bmc.h>
 
-#include "c_test_source_factory.h"
+#include <test-c-gen/expr2cleanc.h>
+
+#include <test-c-gen/c_test_file.h>
+#include <test-c-gen/function_parameter_builder.h>
+#include <test-c-gen/function_return_builder.h>
 
 #include <goto-programs/interpreter_class.h>
 
-
-/*******************************************************************\
-Function: c_test_case_generatort::generate_tests
-Inputs:
- options - The command line options (used by the interpreter)
- st - The symbol table of the trace
- gf - The goto functions the trace is over
- trace - The trace to be reproduced by this test
- test_idx - The index of this test
- goals - The goals this test will cover
-Outputs: An executable C test harness
-Purpose: To generate a test harness that produces a specific trace
-\*******************************************************************/
-std::string c_test_case_generatort::generate_tests(const optionst &options,
-                                                   const symbol_tablet &symbol_table,
-                                                   const goto_functionst &goto_functions,
-                                                   const goto_tracet &trace,
-                                                   const size_t test_idx,
-                                                   const std::vector<std::string> &goals)
-{
-  test_case_generatort generator=generate_c_test_case_from_inputs;
-  return generate_tests_with_generator(options, symbol_table, goto_functions,
-                                       trace, generator, test_idx, goals);
-}
 
 /*******************************************************************\
 Function: c_test_case_generatort::get_test_function_name
@@ -71,26 +51,24 @@ const std::string c_test_case_generatort::get_test_function_name(
 }
 
 /*******************************************************************\
-Function: c_test_case_generatort::generate_tests_with_generator
+Function: c_test_case_generatort::generate_tests
 Inputs:
  options - The command line options (used by the interpreter)
  st - The symbol table of the trace
  gf - The goto functions the trace is over
  trace - The trace to be reproduced by this test
- generator - The function to use to generate the test code
  test_idx - The index of this test
  goals - The goals this test will cover
 Outputs:  An executable C test harness
 Purpose: To generate hte C test harness for a specific trace with a
          with a specific test generation function
 \*******************************************************************/
-std::string c_test_case_generatort::generate_tests_with_generator(const optionst &options,
-                                                                  const symbol_tablet &symbol_table,
-                                                                  const goto_functionst &goto_functions,
-                                                                  const goto_tracet &trace,
-                                                                  const test_case_generatort generator,
-                                                                  size_t test_idx,
-                                                                  std::vector<std::string> goals_reached)
+std::string c_test_case_generatort::generate_tests(const optionst &options,
+                                                   const symbol_tablet &symbol_table,
+                                                   const goto_functionst &goto_functions,
+                                                   const goto_tracet &trace,
+                                                   const size_t test_idx,
+                                                   const std::vector<std::string> &goals_reached)
 {
   status() << "Producing test " << test_idx << eom;
 
@@ -105,17 +83,83 @@ std::string c_test_case_generatort::generate_tests_with_generator(const optionst
   input_vars=interpreter.load_counter_example_inputs(trace, function_inputs,
                                                        side_effects);
 
-
   // Get the file the entry function is in to include it
   const exprt &entry_func=interpretert::get_entry_function(goto_functions);
   const irep_idt &file_name=entry_func.source_location().get_file();
 
-  std::string test_contents=generator(symbol_table, entry_func,
-                                        get_entry_function_id(goto_functions),
-                                        input_vars, file_name);
+  std::string test_contents=generate_c_test_case_from_inputs(
+        symbol_table, entry_func, get_entry_function_id(goto_functions),
+        input_vars, file_name);
 
-  status() << test_contents << eom;
   return test_contents;
+}
+
+/*******************************************************************\
+Function: generate_c_test_case_from_inputs
+Inputs:
+ st - The symbol table
+ func_call_expr - The entry point for the users code
+ function_id - The ID of the entry point function
+ input_vars - The inputs required for the function to recreate the race
+ file_name - The name of the file we are testing
+Outputs: An executable C test harness
+Purpose: To generate a test harness to reproduce a specific trace
+\*******************************************************************/
+std::string c_test_case_generatort::generate_c_test_case_from_inputs(
+    const symbol_tablet &symbol_table,
+    const exprt & func_call_expr,
+    const irep_idt &function_id,
+    const interpretert::input_varst &input_vars,
+    const irep_idt &file_name)
+{
+  c_test_filet test_file;
+  test_file.emit_standard_includes();
+  test_file.emit_file_include(file_name);
+  test_file.emit_main_method();
+
+  test_file.add_line_at_current_indentation("printf(\"Running tests...\\n\");");
+
+  namespacet ns(symbol_table);
+  expr2cleanct e2c(ns);
+
+  inputst function_inputs=filter_inputs_to_function_parameters(input_vars,
+                                                               func_call_expr);
+
+  std::vector<std::string> input_entries;
+  for(const input_entryt &entry : function_inputs)
+  {
+    function_parameter_buildert function_param(entry, e2c, symbol_table);
+
+    test_file.add_line_at_current_indentation(
+      function_param.get_parameter_declaration());
+
+    input_entries.push_back(function_param.get_parameter_variable_name());
+  }
+
+  function_return_buildert return_builder(input_vars, function_id, e2c);
+
+  if(return_builder.get_function_has_return())
+  {
+    test_file.add_line_at_current_indentation(
+      return_builder.get_return_decleration());
+  }
+
+  test_file.add_function(function_id, input_entries, return_builder);
+
+  if(return_builder.get_function_has_return())
+  {
+    const std::vector<std::string> &assertion_lines=
+      return_builder.get_assertion_lines();
+
+    for(const std::string &assert_line : assertion_lines)
+    {
+      test_file.add_line_at_current_indentation(assert_line);
+    }
+  }
+
+  test_file.end_main_method();
+
+  return test_file.get_file();
 }
 
 /*******************************************************************\
@@ -163,4 +207,38 @@ std::string c_test_case_generatort::sanitize_function_name(
   substitute(isolated_function_name, "<", "_");
 
   return isolated_function_name;
+}
+
+/*******************************************************************\
+ Function: filter_inputs_to_function_parameters
+ Inputs:
+  all_inputs - All the inputs that have come back from the interpreter
+  func_expr - The function call to filter for
+Outputs: The inputs which are parameters for the supplied function
+Purpose: To pull out from the inputs the relevant ones for the function
+         call
+\*******************************************************************/
+inputst c_test_case_generatort::filter_inputs_to_function_parameters(
+    const inputst &all_inputs, const exprt &func_expr)
+{
+  const code_typet::parameterst &params=
+      to_code_type(func_expr.type()).parameters();
+
+  inputst filtered_inputs;
+
+  typedef code_typet::parametert parametert;
+  for(const parametert &parameter : params)
+  {
+    const irep_idt &parameter_identifer=parameter.get_identifier();
+    typedef inputst::const_iterator const_inputs_itert;
+    const_inputs_itert param_iter=all_inputs.find(parameter_identifer);
+
+    // The input vars should include all parameters
+    assert(param_iter!=all_inputs.cend());
+
+    filtered_inputs.insert(input_entryt(parameter_identifer,
+                                        param_iter->second));
+  }
+
+  return filtered_inputs;
 }
--- src/test-c-gen/c_test_case_generator.h
+++ src/test-c-gen/c_test_case_generator.h
@@ -15,19 +15,12 @@
 #include <util/message.h>
 
 #include <goto-programs/interpreter_class.h>
-
-typedef std::function<std::string(
-    const symbol_tablet &, // the symbol table for the trace
-    const exprt &, // the expression of the function call
-    const irep_idt &, // the id of the function
-    const interpretert::input_varst &, // the input values for the function
-    const irep_idt & // The file name the users function is in
-    )> test_case_generatort;
+#include <test-c-gen/input_entry.h>
 
 class c_test_case_generatort : public messaget
 {
 public:
-  c_test_case_generatort(message_handlert & _message_handler):
+  c_test_case_generatort(message_handlert &_message_handler):
     messaget(_message_handler)
 {
 }
@@ -45,19 +38,19 @@ public:
 
 
 private:
-  std::string generate_tests_with_generator(const class optionst &options,
-                                            const class symbol_tablet &symbol_table,
-                                            const class goto_functionst &goto_functions,
-                                            const class goto_tracet &trace,
-                                            const test_case_generatort generator,
-                                            size_t test_idx,
-                                            std::vector<std::string> goals_reached);
+  std::string generate_c_test_case_from_inputs(const symbol_tablet &symbol_table,
+                                               const exprt & func_call_expr,
+                                               const irep_idt &function_id,
+                                               const interpretert::input_varst &input_vars,
+                                               const irep_idt &file_name);
 
   const irep_idt get_entry_function_id(const class goto_functionst& gf);
 
   const irep_idt get_calling_function_name(const class exprt &func_expr);
 
   std::string sanitize_function_name(const std::string called_function_name);
+  inputst filter_inputs_to_function_parameters(const inputst &all_inputs,
+                                               const exprt &func_expr);
 };
 
 #endif
