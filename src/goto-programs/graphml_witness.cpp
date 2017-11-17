6e66795653e744ab59110f0d96c7166960edd0f4
Marek Trtik <marek.trtik@diffblue.com>
2017-11-17 11:56:05 +0000

    Using cache to speed up generation of grapml file.

--- src/goto-programs/graphml_witness.cpp
+++ src/goto-programs/graphml_witness.cpp
@@ -17,6 +17,8 @@ Author: Daniel Kroening
 #include <util/arith_tools.h>
 #include <util/prefix.h>
 #include <util/ssa_expr.h>
+#include <util/std_pair_hash.h>
+#include <unordered_map>
 
 #include <langapi/language_util.h>
 
@@ -52,6 +54,13 @@ std::string graphml_witnesst::convert_assign_rec(
   const irep_idt &identifier,
   const code_assignt &assign)
 {
+  static std::unordered_map<std::pair<unsigned int, const irept::dt *>, std::string> cache;
+  {
+      const auto cit = cache.find({identifier.get_no(), &assign.read()});
+      if(cit != cache.end())
+          return cit->second;
+  }
+
   std::string result;
 
   if(assign.rhs().id()==ID_array)
@@ -136,6 +145,7 @@ std::string graphml_witnesst::convert_assign_rec(
     result=lhs+" = "+from_expr(ns, identifier, clean_rhs)+";";
   }
 
+  cache.insert({{identifier.get_no(), &assign.read()}, result});
   return result;
 }
 
