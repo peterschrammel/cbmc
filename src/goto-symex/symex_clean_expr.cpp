8977fd871bb1d09ab0b407b4ced2ad63a94681e8
Peter Schrammel <peter.schrammel@diffblue.com>
2018-10-04 22:59:36 +0100

    Add shadow memory instrumentation

--- src/goto-symex/symex_clean_expr.cpp
+++ src/goto-symex/symex_clean_expr.cpp
@@ -132,7 +132,12 @@ void goto_symext::process_array_expr(statet &state, exprt &expr)
   symex_dereference_statet symex_dereference_state(state, ns);
 
   value_set_dereferencet dereference(
-    ns, state.symbol_table, symex_dereference_state, language_mode, false, log);
+    ns,
+    state.symbol_table,
+    symex_dereference_state,
+    language_mode,
+    false,
+    log.get_message_handler());
 
   expr = dereference.dereference(expr, symex_config.show_points_to_sets);
   lift_lets(state, expr);
@@ -183,13 +188,10 @@ void goto_symext::lift_let(statet &state, const let_exprt &let_expr)
   do_simplify(let_value);
 
   exprt::operandst value_assignment_guard;
-  symex_assignt{
-    state, symex_targett::assignment_typet::HIDDEN, ns, symex_config, target}
-    .assign_symbol(
-      to_ssa_expr(state.rename<L1>(let_expr.symbol(), ns).get()),
-      expr_skeletont{},
-      let_value,
-      value_assignment_guard);
+  symex_assignt{*this, state,        symex_targett::assignment_typet::HIDDEN,
+                ns,    symex_config, target}
+      .assign_symbol(to_ssa_expr(state.rename<L1>(let_expr.symbol(), ns).get()),
+                     expr_skeletont{}, let_value, value_assignment_guard);
 
   // Schedule the bound variable to be cleaned up at the end of symex_step:
   instruction_local_symbols.push_back(let_expr.symbol());
