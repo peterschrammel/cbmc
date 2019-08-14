cf1993183f2d0e421aaea086310e1ae974cc3200
Peter Schrammel <peter.schrammel@diffblue.com>
2019-08-14 23:07:18 +0100

    fixup-tests

--- regression/cbmc-shadow-memory/dynamic-per-object1/main.c
+++ regression/cbmc-shadow-memory/dynamic-per-object1/main.c
@@ -13,11 +13,11 @@ int test_struct()
   assert(__CPROVER_get_field(y, "field1") == 0);
   assert(__CPROVER_get_field(&y->x1, "field1") == 0);
   assert(__CPROVER_get_field(&y->x2, "field1") == 0);
-  __CPROVER_set_field(&y, "field1", 3);
+  __CPROVER_set_field(y, "field1", 3);
   assert(__CPROVER_get_field(y, "field1") == 3);
   assert(__CPROVER_get_field(&y->x1, "field1") == 3);
   assert(__CPROVER_get_field(&y->x2, "field1") == 3);
-  __CPROVER_set_field(&y.x2, "field1", 5);
+  __CPROVER_set_field(&y->x2, "field1", 5);
   assert(__CPROVER_get_field(y, "field1") == 5);
   assert(__CPROVER_get_field(&y->x1, "field1") == 5);
   assert(__CPROVER_get_field(&y->x2, "field1") == 5);
