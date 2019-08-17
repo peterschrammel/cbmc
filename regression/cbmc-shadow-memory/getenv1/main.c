a257628d4282363aee36df171104b01411f77038
Peter Schrammel <peter.schrammel@diffblue.com>
2019-08-17 20:57:43 +0100

    Per object tests

--- regression/cbmc-shadow-memory/getenv1/main.c
+++ regression/cbmc-shadow-memory/getenv1/main.c
@@ -2,7 +2,7 @@
 
 void main()
 {
-  __CPROVER_field_decl_local("dr_write", (_Bool) 0);
-  char * env = getenv("PATH");
+  __CPROVER_field_decl_global("dr_write", (_Bool) 0);
+  char *env = getenv("PATH");
   assert(__CPROVER_get_field(env, "dr_write") == 0);
 }
