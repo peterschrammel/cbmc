20cd03edde743ad4f1c7673b02588b816207947f
Peter Schrammel <peter.schrammel@diffblue.com>
2019-08-19 21:05:04 +0100

    Fix filtering by value set

--- regression/cbmc-shadow-memory/global-per-object1/main.c
+++ regression/cbmc-shadow-memory/global-per-object1/main.c
@@ -12,6 +12,7 @@ struct struct1 y;
 double z;
 
 short a[2];
+short c[2];
 
 int test_struct()
 {
@@ -50,10 +51,33 @@ int test_array()
   assert(__CPROVER_get_field(&a[1], "field1") == 5);
 }
 
+void test_alias()
+{
+  short *ac;
+  int x;
+  __CPROVER_assume(x > 0);
+  if(x)
+    ac = c;
+  else
+    ac = a;
+  assert(__CPROVER_get_field(ac, "field1") == 0);
+  assert(__CPROVER_get_field(&ac[0], "field1") == 0);
+  assert(__CPROVER_get_field(&ac[1], "field1") == 0);
+  __CPROVER_set_field(ac, "field1", 3);
+  assert(__CPROVER_get_field(ac, "field1") == 3);
+  assert(__CPROVER_get_field(&ac[0], "field1") == 3);
+  assert(__CPROVER_get_field(&ac[1], "field1") == 3);
+  __CPROVER_set_field(&ac[1], "field1", 5);
+  assert(__CPROVER_get_field(ac, "field1") == 5);
+  assert(__CPROVER_get_field(&ac[0], "field1") == 5);
+  assert(__CPROVER_get_field(&ac[1], "field1") == 5);
+}
+
 int main()
 {
   __CPROVER_field_decl_global("field1", (int)0);
-  // test_struct();
+  test_struct();
   test_array();
   test_scalar();
+  test_alias();
 }
