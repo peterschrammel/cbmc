88c1d2ad745af72a865a94b8e19320842f00547f
Peter Schrammel <peter.schrammel@diffblue.com>
2020-02-15 17:37:32 +0000

    Output paths requiring solver

--- src/goto-checker/single_path_symex_only_checker.h
+++ src/goto-checker/single_path_symex_only_checker.h
@@ -40,6 +40,7 @@ protected:
   guard_managert guard_manager;
   std::unique_ptr<path_storaget> worklist;
   std::size_t explored_paths = 0;
+  std::size_t paths_requiring_solver = 0;
   std::chrono::duration<double> symex_runtime;
 
   void equation_output(
