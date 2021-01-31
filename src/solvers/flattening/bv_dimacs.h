d09ed61380d71c0dd2d5349705356b099a61a582
Peter Schrammel <peter.schrammel@diffblue.com>
2021-01-31 15:26:01 +0000

    Add --show-dimacs-var

--- src/solvers/flattening/bv_dimacs.h
+++ src/solvers/flattening/bv_dimacs.h
@@ -21,20 +21,26 @@ public:
     const namespacet &_ns,
     propt &_prop,
     message_handlert &message_handler,
-    const std::string &_filename)
-    : bv_pointerst(_ns, _prop, message_handler), filename(_filename)
+    const std::string &_filename,
+    const std::list<std::string> &vars_to_show)
+    : bv_pointerst(_ns, _prop, message_handler),
+      filename(_filename),
+      vars_to_show(vars_to_show)
   {
   }
 
   virtual ~bv_dimacst()
   {
+    show_prop_vars();
     write_dimacs();
   }
 
 protected:
   const std::string filename;
+  const std::list<std::string> &vars_to_show;
   bool write_dimacs();
   bool write_dimacs(std::ostream &);
+  void show_prop_vars();
 };
 
 #endif // CPROVER_SOLVERS_FLATTENING_BV_DIMACS_H
