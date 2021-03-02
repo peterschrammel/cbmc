d25e92fab4a906312f8c9770e7d8c3e51b7edca5
František Nečas <frantisek.necas@protonmail.com>
2021-03-02 23:46:25 +0100

    CLI option for leak checks for abort functions
    Add a new CLI option --memory-cleanup-check and add memory leak
    assertions for abort functions only if the option is on.
    
    This complies with the SV-COMP rules where memory leaks on
    __VERIFIER_error calls are checked only for the memory-cleanup property,
    otherwise they are ignored.

--- src/analyses/goto_check.cpp
+++ src/analyses/goto_check.cpp
@@ -38,6 +38,7 @@ public:
     enable_bounds_check=_options.get_bool_option("bounds-check");
     enable_pointer_check=_options.get_bool_option("pointer-check");
     enable_memory_leak_check=_options.get_bool_option("memory-leak-check");
+    enable_memory_cleanup_check=_options.get_bool_option("memory-cleanup-check");
     enable_div_by_zero_check=_options.get_bool_option("div-by-zero-check");
     enable_signed_overflow_check=
       _options.get_bool_option("signed-overflow-check");
@@ -115,6 +116,7 @@ protected:
   bool enable_bounds_check;
   bool enable_pointer_check;
   bool enable_memory_leak_check;
+  bool enable_memory_cleanup_check;
   bool enable_div_by_zero_check;
   bool enable_signed_overflow_check;
   bool enable_unsigned_overflow_check;
@@ -1809,7 +1811,7 @@ void goto_checkt::goto_check(goto_functiont &goto_function)
       // These are further 'exit points' of the program
       const exprt simplified_guard = simplify_expr(i.guard, ns);
       if(
-        enable_memory_leak_check && simplified_guard.is_false() &&
+        enable_memory_cleanup_check && simplified_guard.is_false() &&
         (i.function == "abort" || i.function == "exit" ||
          i.function == "_Exit" ||
          (i.labels.size() == 1 && i.labels.front() == "__VERIFIER_abort")))
