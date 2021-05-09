30174b6ae4414448bfd7b35a182d27882ee0e9d3
Peter Schrammel <peter.schrammel@diffblue.com>
2021-05-09 13:48:36 +0100

    Add union tests

---
+++ regression/cbmc-shadow-memory/union1/main.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+
+union UNIONNAME
+{
+  int x1;
+  struct {
+    char y1;
+    int y2;
+    short y3;
+  } x2;
+};
+
+int main()
+{
+  // field declarations
+  __CPROVER_field_decl_local("field2", (__CPROVER_bitvector[6])0);
+
+  /***********************
+   * Union
+   ***********************/
+
+  union UNIONNAME u;
+
+  assert(__CPROVER_get_field(&(u.x1), "field2") == 0);
+  assert(__CPROVER_get_field(&(u.x2.y1), "field2") == 0);
+  assert(__CPROVER_get_field(&(u.x2.y2), "field2") == 0);
+  assert(__CPROVER_get_field(&(u.x2.y3), "field2") == 0);
+
+  __CPROVER_set_field(&(u.x1), "field2", 1);
+  assert(__CPROVER_get_field(&(u.x1), "field2") == 1);
+  assert(__CPROVER_get_field(&(u.x2.y1), "field2") == 1);
+  // assert(__CPROVER_get_field(&(u.x2.y2), "field2") == 1);
+  assert(__CPROVER_get_field(&(u.x2.y3), "field2") == 0);
+
+  __CPROVER_set_field(&(u.x2.y1), "field2", 2);
+  assert(__CPROVER_get_field(&(u.x1), "field2") == 2);
+  assert(__CPROVER_get_field(&(u.x2.y1), "field2") == 2);
+  // assert(__CPROVER_get_field(&(u.x2.y2), "field2") == 2);
+  assert(__CPROVER_get_field(&(u.x2.y3), "field2") == 0);
+
+  __CPROVER_set_field(&(u.x2.y2), "field2", 3);
+  // assert(__CPROVER_get_field(&(u.x1), "field2") == 3);
+  assert(__CPROVER_get_field(&(u.x2.y1), "field2") == 2);
+  assert(__CPROVER_get_field(&(u.x2.y2), "field2") == 3);
+  assert(__CPROVER_get_field(&(u.x2.y3), "field2") == 0);
+
+  __CPROVER_set_field(&(u.x2.y3), "field2", 4);
+  // assert(__CPROVER_get_field(&(u.x1), "field2") == 3);
+  assert(__CPROVER_get_field(&(u.x2.y1), "field2") == 2);
+  assert(__CPROVER_get_field(&(u.x2.y2), "field2") == 3);
+  assert(__CPROVER_get_field(&(u.x2.y3), "field2") == 4);
+}
