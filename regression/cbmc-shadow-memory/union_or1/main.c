44f194244e25055a5928d00c9082881ca93ab62c
Peter Schrammel <peter.schrammel@diffblue.com>
2021-06-06 16:37:59 +0100

    Consider padding in union_or test

--- regression/cbmc-shadow-memory/union_or1/main.c
+++ regression/cbmc-shadow-memory/union_or1/main.c
@@ -5,7 +5,8 @@ union UNIONNAME
   int x1;
   struct {
     char y1;
-    int y2;
+    // char padding;
+    short y2;
     short y3;
   } x2;
   char x3[3];
@@ -21,7 +22,6 @@ int main()
    ***********************/
 
   union UNIONNAME u;
-
   assert(__CPROVER_get_field(&u, "field2") == 0);
   assert(__CPROVER_get_field(&(u.x1), "field2") == 0);
   assert(__CPROVER_get_field(&(u.x2), "field2") == 0);
@@ -49,7 +49,7 @@ int main()
   assert(__CPROVER_get_field(&u, "field2") == 3);
   assert(__CPROVER_get_field(&(u.x1), "field2") == 3);
   assert(__CPROVER_get_field(&(u.x2), "field2") == 3);
-  assert(__CPROVER_get_field(&(u.x2.y1), "field2") == 3);
+  assert(__CPROVER_get_field(&(u.x2.y1), "field2") == 2);
   assert(__CPROVER_get_field(&(u.x2.y2), "field2") == 1);
   assert(__CPROVER_get_field(&(u.x2.y3), "field2") == 0);
   //assert(__CPROVER_get_field(u.x3, "field2") == 3);
@@ -60,24 +60,24 @@ int main()
   __CPROVER_set_field(&(u.x2.y2), "field2", 4);
   assert(__CPROVER_get_field(&u, "field2") == 7);
   assert(__CPROVER_get_field(&(u.x1), "field2") == 7);
-  assert(__CPROVER_get_field(&(u.x2), "field2") == 7);
-  assert(__CPROVER_get_field(&(u.x2.y1), "field2") == 3);
-  assert(__CPROVER_get_field(&(u.x2.y2), "field2") == 5);
+  assert(__CPROVER_get_field(&(u.x2), "field2") == 6);
+  assert(__CPROVER_get_field(&(u.x2.y1), "field2") == 2);
+  assert(__CPROVER_get_field(&(u.x2.y2), "field2") == 4);
   assert(__CPROVER_get_field(&(u.x2.y3), "field2") == 0);
   //assert(__CPROVER_get_field(u.x3, "field2") == 7);
-  assert(__CPROVER_get_field(&(u.x3[0]), "field2") == 3);
-  assert(__CPROVER_get_field(&(u.x3[1]), "field2") == 5);
-  assert(__CPROVER_get_field(&(u.x3[2]), "field2") == 5);
+  assert(__CPROVER_get_field(&(u.x3[0]), "field2") == 2);
+  assert(__CPROVER_get_field(&(u.x3[1]), "field2") == 1);
+  assert(__CPROVER_get_field(&(u.x3[2]), "field2") == 4);
 
   __CPROVER_set_field(&(u.x2.y3), "field2", 8);
   assert(__CPROVER_get_field(&u, "field2") == 15);
   assert(__CPROVER_get_field(&(u.x1), "field2") == 7);
-  assert(__CPROVER_get_field(&(u.x2), "field2") == 15);
-  assert(__CPROVER_get_field(&(u.x2.y1), "field2") == 3);
-  assert(__CPROVER_get_field(&(u.x2.y2), "field2") == 5);
+  assert(__CPROVER_get_field(&(u.x2), "field2") == 14);
+  assert(__CPROVER_get_field(&(u.x2.y1), "field2") == 2);
+  assert(__CPROVER_get_field(&(u.x2.y2), "field2") == 4);
   assert(__CPROVER_get_field(&(u.x2.y3), "field2") == 8);
   //assert(__CPROVER_get_field(u.x3, "field2") == 7);
-  assert(__CPROVER_get_field(&(u.x3[0]), "field2") == 3);
-  assert(__CPROVER_get_field(&(u.x3[1]), "field2") == 5);
-  assert(__CPROVER_get_field(&(u.x3[2]), "field2") == 5);
+  assert(__CPROVER_get_field(&(u.x3[0]), "field2") == 2);
+  assert(__CPROVER_get_field(&(u.x3[1]), "field2") == 1);
+  assert(__CPROVER_get_field(&(u.x3[2]), "field2") == 4);
 }
