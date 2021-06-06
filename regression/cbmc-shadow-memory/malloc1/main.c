3791c4c54daa3990ed3c79d90c9482b4a4959dee
Peter Schrammel <peter.schrammel@diffblue.com>
2021-06-06 18:42:14 +0100

    Fix up tests to work with per byte replication semantics

--- regression/cbmc-shadow-memory/malloc1/main.c
+++ regression/cbmc-shadow-memory/malloc1/main.c
@@ -17,7 +17,7 @@ void __cs_init_scalar(void *__cs_var, unsigned int __cs_size)
 
 void main()
 {
-  __CPROVER_field_decl_global("dr_write", (_Bool) 0);
+  __CPROVER_field_decl_global("dr_write", (unsigned __CPROVER_bitvector[2]) 0);
 
   static char *local_Lkeys_thread_ver;
   local_Lkeys_thread_ver = (char *) __cs_safe_malloc((sizeof(char)) * 5l);
@@ -30,9 +30,9 @@ void main()
   assert(__CPROVER_get_field(&local_main_Lkeys, "dr_write") == 0);
 
   __CPROVER_set_field(local_Lkeys_thread_ver + 1, "dr_write", 1);
-  __CPROVER_set_field(&local_main_Lkeys, "dr_write", 2);
+  __CPROVER_set_field(&local_main_Lkeys, "dr_write", 2u);
 
   assert(__CPROVER_get_field(local_Lkeys_thread_ver, "dr_write") == 0);
   assert(__CPROVER_get_field(local_Lkeys_thread_ver + 1, "dr_write") == 1);
-  assert(__CPROVER_get_field(&local_main_Lkeys, "dr_write") == 2);
+  assert(__CPROVER_get_field(&local_main_Lkeys, "dr_write") == 2u);
 }
