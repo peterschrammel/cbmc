0ae51f055e7fedebae00ca416c4413ec5cd0fcd8
Peter Schrammel <peter.schrammel@diffblue.com>
2025-01-13 16:35:21 +0000

    attempt

--- src/goto-symex/lazy_c_seq.cpp
+++ src/goto-symex/lazy_c_seq.cpp
@@ -9,4 +9,20 @@ void lazy_c_seqt::operator()(
 {
   messaget log{message_handler};
   log.statistics() << "Adding LazyCSeq constraints" << messaget::eom;
+
+  for(const auto &step : equation.SSA_steps)
+  {
+    if(step.is_shared_write())
+    {
+      log.warning() << "Thread: " << step.source.thread_nr
+                    << "Write: " << step.source.pc->location_number
+                    << messaget::eom;
+    }
+    if(step.is_shared_read())
+    {
+      log.warning() << "Thread: " << step.source.thread_nr
+                    << "Read: " << step.source.pc->location_number
+                    << messaget::eom;
+    }
+  }
 }
