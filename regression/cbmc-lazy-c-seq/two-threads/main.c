cfa0b93b1a51cb1f968b1e109801bad6d4c18634
Peter Schrammel <peter.schrammel@diffblue.com>
2025-01-24 12:00:10 +0000

    fix benchmark two-threads

--- regression/cbmc-lazy-c-seq/two-threads/main.c
+++ regression/cbmc-lazy-c-seq/two-threads/main.c
@@ -5,32 +5,22 @@
 extern void __VERIFIER_atomic_begin(void);
 extern void __VERIFIER_atomic_end(void);
 
-int shared;
+int x;
 
 void *t1(void *arg) {
-  int value_t1;
-  value_t1 = 5;
-
-  __VERIFIER_atomic_begin();
-  value_t1 += shared;
-  __VERIFIER_atomic_end();
+  int a;
+  a = x + 1;
+  x = a;
 }
 
 void *t2(void *arg) {
-  int value_t2;
-  value_t2 = 3;
-
-  __VERIFIER_atomic_begin();
-  shared += value_t2;
-  __VERIFIER_atomic_end();
+  int b;
+  b = x;
+  x = 42;
 }
 
 int main(void) {
   pthread_t id1, id2;
-
-  __VERIFIER_atomic_begin();
-  shared = 10;
-  __VERIFIER_atomic_end();
 
   pthread_create(&id1, NULL, t1, NULL);
   pthread_create(&id2, NULL, t2, NULL);
@@ -38,5 +28,5 @@ int main(void) {
   pthread_join(id1, NULL);
   pthread_join(id2, NULL);
 
-  assert(0);
+  assert(x == 42 || x == 43);
 }
