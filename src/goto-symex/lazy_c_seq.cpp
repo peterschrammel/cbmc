f88f7a46f542840757bb184a99a5bb24cb0e2d88
Paolo Di Biase <paolodibiase1@gmail.com>
2025-02-07 12:53:04 +0100

    changed global_variable to set and last_write_of_previous_round to const_iterator

--- src/goto-symex/lazy_c_seq.cpp
+++ src/goto-symex/lazy_c_seq.cpp
@@ -27,14 +27,14 @@ void lazy_c_seqt::operator()(
     writes;
   collect_reads_and_writes(equation.SSA_steps, reads, writes, message_handler);
 
-  create_write_constraints(equation.SSA_steps, writes, message_handler);
+  create_write_constraints(equation, writes, message_handler);
 
   exprt tmp;
   simplify(tmp, ns);
 }
 
 void lazy_c_seqt::create_write_constraints(
-  symex_target_equationt::SSA_stepst &ssa_steps,
+  symex_target_equationt &equation,
   const std::unordered_map<
     unsigned,
     std::vector<symex_target_equationt::SSA_stepst::const_iterator>> &writes,
@@ -43,18 +43,11 @@ void lazy_c_seqt::create_write_constraints(
   messaget log{message_handler};
 
   // last write of main thread
-  exprt last_write_of_previous_round;
-  std::vector<irep_idt> global_variables;
+  symex_target_equationt::SSA_stepst::const_iterator last_write_of_previous_round;
+  std::unordered_set<irep_idt> global_variables;
   for(auto &s_it : writes.at(0))
   {
-    bool contain = false;
-    for(irep_idt variable : global_variables)
-    {
-      if(variable == s_it->ssa_lhs.get_object_name())
-        contain = true;
-    }
-    if(!contain)
-      global_variables.push_back(s_it->ssa_lhs.get_object_name());
+      global_variables.insert(s_it->ssa_lhs.get_object_name());
   }
 
   for(irep_idt variable : global_variables)
@@ -62,13 +55,13 @@ void lazy_c_seqt::create_write_constraints(
     for(auto &s_it : writes.at(0))
     {
       if(s_it->ssa_lhs.get_object_name() == variable)
-        last_write_of_previous_round = s_it->ssa_lhs;
+        last_write_of_previous_round = s_it;
     }
 
     for(std::size_t round = 1; round <= rounds; ++round)
     {
       symex_target_equationt::SSA_stepst::const_iterator
-        last_write_of_current_round;
+        last_write_of_current_round = last_write_of_previous_round;
       for(unsigned thread_nr = 1; thread_nr < writes.size(); ++thread_nr)
       {
         for(auto &s_it : writes.at(thread_nr))
@@ -96,9 +89,10 @@ void lazy_c_seqt::create_write_constraints(
           if_exprt{
             statement_label,
             last_write_of_current_round->ssa_lhs,
-            last_write_of_previous_round}};
+            last_write_of_previous_round->ssa_lhs}};
         log.warning() << format(constraint) << messaget::eom;
-        last_write_of_previous_round = end_of_round_value;
+        equation.constraint(constraint,"write constraint", last_write_of_current_round->source);
+        last_write_of_previous_round = std::prev(equation.SSA_steps.cend());
       }
     }
   }
