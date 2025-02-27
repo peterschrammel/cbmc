883450050b19d973e6f2a994f5c01055fb727fd9
Paolo Di Biase <paolodibiase1@gmail.com>
2025-02-27 17:18:41 +0000

    added constraint to check if a thread is exited

--- src/goto-symex/lazy_c_seq.cpp
+++ src/goto-symex/lazy_c_seq.cpp
@@ -39,6 +39,7 @@ void lazy_c_seqt::operator()(
   std::
     unordered_map<irep_idt, symex_target_equationt::SSA_stepst::const_iterator>
       last_cprover_upadte;
+  symex_target_equationt::SSA_stepst::const_iterator exited_array;
 
   collect_reads_and_writes(
     equation.SSA_steps, reads, main_reads, writes, message_handler);
@@ -51,12 +52,12 @@ void lazy_c_seqt::operator()(
     create_read_constraints(
       equation, reads, writes, last_update, last_update_main, message_handler);
 
-  create_cprover_constraints(equation, last_cprover_upadte, message_handler);
+  create_cprover_constraints(equation, last_cprover_upadte, exited_array, message_handler);
 
   if(!(writes.empty() && reads.empty()))
   {
-    //create_cs_constraint(equation,reads,writes,message_handler);
-    create_reach_constraint(equation, reads, writes, message_handler);
+    create_cs_constraint(equation,reads,writes,message_handler);
+    create_reach_constraint(equation, reads, writes, exited_array, message_handler);
   }
 
   if(!main_reads.empty())
@@ -472,6 +473,7 @@ void lazy_c_seqt::create_reach_constraint(
   std::unordered_map<
     unsigned,
     std::vector<symex_target_equationt::SSA_stepst::const_iterator>> &writes,
+    symex_target_equationt::SSA_stepst::const_iterator &exited_array,
   message_handlert &message_handler)
 {
   messaget log{message_handler};
@@ -479,7 +481,7 @@ void lazy_c_seqt::create_reach_constraint(
   log.warning() << "-------------------REACH--------------------------"
                 << messaget::eom;
 
-  for(auto read : reads)
+  for(auto &read : reads)
   {
     std::string label_name =
       "_L" + std::to_string(read.first->source.pc->location_number);
@@ -508,17 +510,22 @@ void lazy_c_seqt::create_reach_constraint(
     symbol_exprt reach{reach_name, bool_typet{}};
 
     equal_exprt final_constraint{reach, constraint};
+    simplify(final_constraint, ns);
     log.warning() << format(final_constraint) << messaget::eom;
     equation.constraint(
       final_constraint, "reach constraint", read.first->source);
 
-    log.warning() << format(reach) << messaget::eom;
-    equation.constraint(reach, "reach constraint", read.first->source);
+    index_exprt exited{exited_array->ssa_lhs, from_integer({read.first->source.thread_nr}, unsignedbv_typet{8})};
+    equal_exprt reach_constraint{reach, exited};
+    simplify(reach_constraint, ns);
+    log.warning() << format(reach_constraint) << messaget::eom;
+    equation.constraint(
+      reach_constraint, "reach constraint", read.first->source);
   }
 
   for(unsigned thread_nr = 1; thread_nr < writes.size(); ++thread_nr)
   {
-    for(const auto &write : writes.at(thread_nr))
+    for(auto &write : writes.at(thread_nr))
     {
       std::string label_name =
         "_L" + std::to_string(write->source.pc->location_number);
@@ -545,10 +552,15 @@ void lazy_c_seqt::create_reach_constraint(
       symbol_exprt reach{reach_name, bool_typet{}};
 
       equal_exprt final_constraint{reach, constraint};
+      simplify(final_constraint, ns);
       log.warning() << format(final_constraint) << messaget::eom;
       equation.constraint(final_constraint, "reach constraint", write->source);
-      log.warning() << format(reach) << messaget::eom;
-      equation.constraint(reach, "reach constraint", write->source);
+
+      index_exprt exited{exited_array->ssa_lhs, from_integer({thread_nr}, unsignedbv_typet{8})};
+      equal_exprt reach_constraint{reach, exited};
+      simplify(reach_constraint, ns);
+      log.warning() << format(reach_constraint) << messaget::eom;
+      equation.constraint(reach_constraint, "reach constraint", write->source);
     }
   }
 }
@@ -728,6 +740,7 @@ void lazy_c_seqt::create_cprover_constraints(
   std::unordered_map<
     irep_idt,
     symex_target_equationt::SSA_stepst::const_iterator> &last_cprover_upadte,
+    symex_target_equationt::SSA_stepst::const_iterator &exited_array,
   message_handlert &message_handler)
 {
   //std::vector<symex_target_equationt::SSA_stepst::const_iterator> atomic_op;
@@ -782,6 +795,8 @@ void lazy_c_seqt::create_cprover_constraints(
     }
     if(s_it->is_shared_write())
     {
+      if(s_it->ssa_lhs.get_object_name() == "__CPROVER_threads_exited")
+        exited_array = s_it;
       last_cprover_upadte[s_it->ssa_lhs.get_object_name()] = s_it;
     }
     if(s_it->is_shared_read())
