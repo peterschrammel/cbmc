23f397059cde295977a01cd432a4a2078c7b9806
Paolo Di Biase <paolodibiase1@gmail.com>
2025-02-28 11:09:16 +0000

    added 2 tests in lazy-c-seq regression

---
+++ regression/cbmc-lazy-c-seq/two-threads-fail/main.c
@@ -0,0 +1,41 @@
+#include <assert.h>
+#include <pthread.h>
+#include <stdio.h>
+
+extern void __VERIFIER_atomic_begin(void);
+extern void __VERIFIER_atomic_end(void);
+
+int x = 42;
+
+void *t1(void *arg)
+{
+  int a;
+  a = x + 1;
+  x = a;
+  a = x--;
+  x--;
+  a = x++;
+  x++;
+}
+
+void *t2(void *arg)
+{
+  int b;
+  b = x;
+  x = 42;
+}
+
+int main(void)
+{
+  pthread_t id1, id2;
+  //x=x;
+
+  pthread_create(&id1, NULL, t1, NULL);
+  pthread_create(&id2, NULL, t2, NULL);
+
+  pthread_join(id1, NULL);
+  pthread_join(id2, NULL);
+  //x=x;
+
+  assert(x == 42 || x == 43);
+}
